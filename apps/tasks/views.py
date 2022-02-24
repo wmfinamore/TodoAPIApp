be5be3151442479da77bab644c6a58e691bf3e6b
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-02-24 17:23:20 -0300

    Create viewto get tasks and create a new task

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import TaskSerializer
+from .models import Task
+
+
+@api_view(['GET'])
+def all_tasks(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def create_task(request):
+    data = request.data
+    serializer = TaskSerializer(data=data)
+    if serializer.is_valid():
+        return Response(serializer.data, status=status.HTTP_200_OK)
