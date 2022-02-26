0bf97244f164d641a36ac87d7dd3a60d86bddc3e
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-02-26 16:37:35 -0300

    Defined correct url for create new tasks. Clear the text after the submit

--- apps/kivy/main.py
+++ apps/kivy/main.py
@@ -44,9 +44,9 @@ class AddNewForm(Widget):
 
     def submit_input(self):
         self.input = self.text_input.text
-        post = requests.post('http://127.0.0.1:8000', json={'name': self.input})
+        post = requests.post('http://127.0.0.1:8000/create', json={'name': self.input})
 
-        self.input = ''
+        self.text_input.text = ''
 
 
 class AddScreen(Screen):
