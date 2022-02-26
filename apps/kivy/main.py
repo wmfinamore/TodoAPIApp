1dbcb7fc1a89860f35a961b3787af4dff60a1616
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-02-26 14:22:05 -0300

    Improved the app layout

--- apps/kivy/main.py
+++ apps/kivy/main.py
@@ -1,6 +1,19 @@
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.boxlayout import BoxLayout
 
 
+# menu
+class Menu(BoxLayout):
+    pass
+
+
+# Screen Management
+class ScreenManagement(ScreenManager):
+    pass
+
+
+# app class
 class TodoApp(App):
     pass
 
