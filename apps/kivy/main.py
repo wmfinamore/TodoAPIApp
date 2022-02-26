c3506b20661aab43c46d13f9b76dc148b015940e
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-02-26 14:41:30 -0300

    Implemented class RecycleView to request data from backend

--- apps/kivy/main.py
+++ apps/kivy/main.py
@@ -2,11 +2,29 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
 
+import requests
+from kivy.clock import Clock
+from kivy.uix.recycleview import RecycleView
+
 
 # menu
 class Menu(BoxLayout):
     pass
 
+
+class MyRecycleView(RecycleView):
+    def __init__(self, **kwargs):
+        super(MyRecycleView, self).__init__(**kwargs)
+        self.load_data()
+        Clock.schedule_interval(self.load_data, 1)
+    def load_data(self, *args):
+        store = requests.get('http://127.0.0.1:8000/').json()
+
+        list_data = []
+        from item in store:
+            list_data.append({'text': item['name']})
+
+        self.data= list_data
 
 class HomeScreen(Screen):
     pass
