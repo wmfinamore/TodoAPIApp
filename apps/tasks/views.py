d7ba499547f57ea4c49323b3b086b852ba16195f
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-02-27 21:30:48 -0300

    Added permission in the endpoints

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -5,11 +5,14 @@ from rest_framework.response import Response
 from .serializers import TaskSerializer
 from .models import Task
 
+from rest_framework import permissions
+
 
 # list tasks
 @api_view(['GET'])
 def all_tasks(request):
     tasks = Task.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -18,6 +21,7 @@ def all_tasks(request):
 @api_view(['POST'])
 def create_task(request):
     data = request.data
+    permission_classes = [permissions.IsAuthenticated]
     serializer = TaskSerializer(data=data)
     if serializer.is_valid():
         serializer.create(data)
