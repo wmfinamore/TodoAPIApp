abce863fc7ea195fd625c203082c98d08b7c8bde
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-03-05 20:32:21 -0300

    Fixed function to use permission_classes decorator

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,27 +1,27 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 from .serializers import TaskSerializer
 from .models import Task
 
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
 
 
 # list tasks
 @api_view(['GET'])
+@permission_classes([IsAuthenticated, ])
 def all_tasks(request):
     tasks = Task.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 # create
 @api_view(['POST'])
+@permission_classes([IsAuthenticated, ])
 def create_task(request):
     data = request.data
-    permission_classes = [permissions.IsAuthenticated]
     serializer = TaskSerializer(data=data)
     if serializer.is_valid():
         serializer.create(data)
